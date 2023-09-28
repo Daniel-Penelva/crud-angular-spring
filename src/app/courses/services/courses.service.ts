cb9bfb913f0b1b2efa9643c58b61b0e0b5f2869b
Daniel Penelva <d4n.andrade@gmail.com>
2023-09-27 21:02:46 -0300

    Atualizar Curso: Salvando o Formulário

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -18,22 +18,36 @@ export class CoursesService {
     return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
       //delay(5000),
-      tap((courses) => console.log(courses))
+      //tap((courses) => console.log(courses))
     );
   }
 
-  // Método para salvar curso no formulario - vai passar o método post e os dados nome e categoria pela variável record (body = corpo)
-  // Lembrando que retorna um Observable porque ele trabalha com HttpClient -> por exemplo, save(record: Course): Observable<any>{...}
-  // Partial<Course> é usado para indicar que o objeto passado como parâmetro contém apenas um subconjunto das propriedades da classe Course. 
-  //Isso é útil quando você quer permitir que apenas algumas propriedades do objeto sejam definidas
   save(record: Partial<Course>){
     //console.log(record);
 
-    return this.httpClient.post<Course>(this.API, record).pipe(first());
+    // Verifica se existe id, e se existir apenas atualiza, caso não exista curso ele cria.
+    if(record._id){
+      //console.log('update');
+      return this.update(record);
+    }
+    //console.log('create');
+    return this.create(record);
   }
 
   // Método para buscar por id
   loadById(id: string){
     return this.httpClient.get<Course>(`${this.API}/${id}`);
+  }
+
+  // Método para criar curso no formulario - vai passar o método post e os dados nome e categoria pela variável record (body = corpo)
+  // Lembrando que retorna um Observable porque ele trabalha com HttpClient -> por exemplo, save(record: Course): Observable<any>{...}
+  // Partial<Course> é usado para indicar que o objeto passado como parâmetro contém apenas um subconjunto das propriedades da classe Course. 
+  //Isso é útil quando você quer permitir que apenas algumas propriedades do objeto sejam definidas
+  private create(record: Partial<Course>){
+    return this.httpClient.post<Course>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Course>){
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
   }
 }
